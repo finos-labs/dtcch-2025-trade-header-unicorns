c0c396a5a3f8d7c49a61bcb9b5692b5de5511bd6
Lucia_Cortes <lcortes@tradeheader.com>
2025-02-05 19:46:40 +0100

    Added isin enrichment in news-to-iso
    Signed-off-by: Lucia_Cortes <lcortes@tradeheader.com>

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
 
 def read_file(file_path):
     try:
@@ -10,3 +12,47 @@ def read_file(file_path):
         print(f"Error reading the file {file_path}: {e}")
         return None
     
+  
+def extract_ticker_from_xml(xml_string):
+
+    namespace = {'ns': 'urn:iso:std:iso:20022:tech:xsd:DRAFT1seev.031.002.15'}
+    root = ET.fromstring(xml_string)
+    ticker_element = root.find(".//ns:UndrlygScty/ns:FinInstrmId/ns:OthrId/ns:Id", namespace)
+    return ticker_element.text if ticker_element is not None else None
+
+
+def get_isin_from_ticker(ticker):
+
+    csv_path="data/isin_enrichment/isin_database.csv"
+
+    try:
+        df = pd.read_csv(csv_path, delimiter=";")
+        isin = df.loc[df['Ticker'] == ticker, 'ISIN']
+        return isin.values[0] if not isin.empty else None
+    except Exception as e:
+        print(f"Error when reading the file: {e}")
+        return None
+
+def insert_isin_into_xml(xml_string, isin):
+
+    namespace = {'ns': 'urn:iso:std:iso:20022:tech:xsd:DRAFT1seev.031.002.15'}
+    ET.register_namespace('', namespace['ns']) 
+    
+    root = ET.fromstring(xml_string)
+    
+    isin_element = root.find(".//ns:UndrlygScty/ns:FinInstrmId/ns:ISIN", namespace)
+    
+    if isin_element is not None:
+        isin_element.text = isin
+    else:
+        print("No se encontró el nodo ISIN en el XML.")
+    
+    return ET.tostring(root, encoding='unicode')
+
+def isin_enrichment(xml_string):
+    ticker = extract_ticker_from_xml(xml_string)
+    isin = get_isin_from_ticker(ticker)
+    return insert_isin_into_xml(xml_string, isin)
+
+    
+
